e903fd8f85259f580a2767a0db0a356391698d90
kalyan <shiva.kalyan.rajavaram@gmail.com>
2021-07-02 12:21:48 -0700

    Sample web server created using node js

--- index.js
+++ index.js
@@ -62,6 +62,6 @@ const server = http.createServer((req, res) => {
   });
 });
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
